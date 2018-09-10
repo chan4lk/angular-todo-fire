2d73a47e734606dfd698697d5b4ae229ff0720e6
Chandima Ranaweera <chan4lk@gmail.com>
2018-09-10 21:26:09 +0530

    initial commit

---
+++ src/app/todo/shared/todo.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { AngularFireDatabase, AngularFireList } from '@angular/fire/database';
+@Injectable({
+  providedIn: 'root'
+})
+export class TodoService {
+  todoList: AngularFireList<any>;
+  constructor(private firesebaseDB: AngularFireDatabase) { }
+
+  getTodoList() {
+    this.todoList = this.firesebaseDB.list('titles');
+    return this.todoList;
+  }
+
+  addTitle(title: string) {
+    this.todoList.push({
+      title: title,
+      isChecked: false
+    });
+  }
+
+  checkOrUncheckTitle($key: string, flag: boolean) {
+    this.todoList.update($key, { isChecked: flag });
+  }
+
+  removeTitle($key: string) {
+    this.todoList.remove($key);
+  }
+}
