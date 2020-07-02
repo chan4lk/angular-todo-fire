88bfb878755d960b07653a29d8355f303b568c5e
Chandima Ranaweera <chan4lk@gmail.com>
2020-07-03 01:54:50 +0530

    github workflow added

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAiGXEdlF3y7HaPRRUXpIfqoOPWPFn6gcc",
+    authDomain: "todo-app-training.firebaseapp.com",
+    databaseURL: "https://todo-app-training.firebaseio.com",
+    projectId: "todo-app-training",
+    storageBucket: "",
+    messagingSenderId: "509173473143",
+  },
 };
